6c4ef7caf0a157ada2190fa4645a2a8312262f9f
awenHALV <awen56789@sina.com>
2021-08-10 12:06:39 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Hello Vue 3 in CodeSandbox!" /> -->
+  <PopOver :open="false"/>
+  <TudoAndRedo />
+</template>
+
+<script>
+// import HelloWorldVue from "./components/HelloWorld.vue";
+import PopOver from "./components/PopOver.vue";
+import TudoAndRedo from "./components/TudoAndRedo.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld: HelloWorldVue,
+    PopOver: PopOver,
+    TudoAndRedo: TudoAndRedo
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PopOver.vue
@@ -0,0 +1,63 @@
+<template>
+  <button @click="open = true"> 123123123{{open}}</button>
+  <div class="popup" v-if=open>
+    <div class="popup-content" ref="popup">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis aliquid autem reiciendis eius accusamus sequi, ipsam corrupti vel laboriosam necessitatibus sit natus vero sint ullam! Omnis commodi eos accusantium illum?
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+export default {
+  setup() {
+  const open = ref(false) // state of our popup
+  const popup = ref() // template ref
+  // whenever our popup exists, and we click anything BUT it
+  onClickOutside(popup, () => {
+    open.value  = false
+  })
+  return {
+    open,
+    popup
+  }
+}
+
+
+}
+
+// const handelClick = () => {
+//   this.open = !this.open;
+// }
+
+
+</script>
+
+<style scoped>
+  button {
+    border: none;
+    outline: none;
+    margin-right: 10px;
+    background-color: #2ecc71;
+    color: white;
+    padding: 5px 10px;;
+  }
+  .popup {
+    position: fixed;
+    top:;
+    left:;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(, , , 0.1);
+  }
+  .popup-content {
+    min-width: 300px;
+    padding: 20px;
+    width: 30%;
+    background: #fff;
+  }
+</style>
